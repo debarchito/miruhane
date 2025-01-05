fa4c1e1f1bb503cd63b003d40292ae720fb3f3ce
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-01-05 17:34:13 +0530

    Index email and format changes

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,18 +1,23 @@
-import { pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, varchar, timestamp, index } from "drizzle-orm/pg-core";
 
-export const user = pgTable("user", {
-  id: varchar("id", { length: 32 }).primaryKey(),
-  // usernames don't have to be unique; uniqueness is enforced by the id and email
-  username: varchar("username", { length: 32 }).notNull(),
-  // RFC 5321, SMTP Protocol, limits the email address to 254 characters
-  email: varchar("email", { length: 254 }).notNull().unique(),
-  // default output length is 32 + 1/3 of 32 = 43
-  passwordHash: varchar("password_hash", { length: 43 }).notNull(),
-});
+export const user = pgTable(
+  "user",
+  {
+    id: varchar("id", { length: 32 }).primaryKey(),
+    // usernames don't have to be unique; uniqueness is enforced by the id and email
+    username: varchar("username", { length: 32 }).notNull(),
+    // RFC 5321, SMTP Protocol, limits the email address to 254 characters
+    email: varchar("email", { length: 254 }).notNull().unique(),
+    // default output length is 32 + 1/3 of 32 = 43
+    passwordHash: varchar("password_hash", { length: 43 }).notNull(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+  },
+  (table) => [index("idx_email").on(table.email)],
+);
 
 export const session = pgTable("session", {
   id: varchar("id", { length: 32 }).primaryKey(),
-  userId: varchar("username", { length: 32 })
+  userId: varchar("user_id", { length: 32 })
     .notNull()
     .references(() => user.id),
   expiresAt: timestamp("expires_at", { withTimezone: true, mode: "date" }).notNull(),
