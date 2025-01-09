52ad753d74d4ac4b355802b8ba0d7a2a895b15e2
Subhajit Roy <darrang48@gmail.com>
2025-01-09 12:35:04 +0000

    Created User Interface with Web Speech API which Records words in Real Time, so processing is fast

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,6 +5,28 @@ declare global {
       session: import("$lib/server/auth").SessionValidationResult["session"];
     }
   }
+
+  interface SpeechRecognitionEvent extends Event {
+    resultIndex: number;
+    results: SpeechRecognitionResultList;
+  }
+
+  interface SpeechRecognitionResultList extends Array<SpeechRecognitionResult> {}
+
+  interface SpeechRecognitionErrorEvent extends Event {
+    error: string;
+    message?: string;
+  }  
+
+  interface SpeechRecognitionResult {
+    transcript: string;
+    confidence: number;
+  }
+
+  interface Window {
+    SpeechRecognition: any;
+    webkitSpeechRecognition: any;
+  }
 }
 
 export {};
